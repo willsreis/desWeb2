6045c95acd8964cfd3a5e9efccf9dec07b121cc3
willsreis <williamsilva.r@gmail.com>
2024-08-23 08:06:14 -0300

    primeiro exercicio js

--- aula1-js/exercAula01.js
+++ aula1-js/exercAula01.js
@@ -4,7 +4,7 @@ const prompt = PromptSync()
 let fatorMultip = Number(prompt('Digite o valor a ser multiplicado: '))
 let result = 0
 
-for (let i =0; i <= 10; i++){
+for (let i =1; i <= 10; i++){
     result = fatorMultip * i
     console.log(` ${fatorMultip} x ${i} = ${result}`)
 }
